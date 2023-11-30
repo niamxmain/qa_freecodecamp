a2f61c719d4bdd3148f536e7effe5b5e0e089348
niamx <muhammadniam1999@gmail.com>
2023-12-01 00:06:58 +0700

    task 6

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -35,15 +35,15 @@ suite('Unit Tests', function () {
     test('#equal, #notEqual', function () {
       assert.equal(12, '12', 'Numbers are coerced into strings with ==');
       assert.notEqual({ value: 1 }, { value: 1 }, '== compares object references');
-      assert.notEqual(6 * '2', '12');
+      assert.equal(6 * '2', '12');
       assert.notEqual(6 + '2', '12');
     });
     // #6
     test('#strictEqual, #notStrictEqual', function () {
-      assert.fail(6, '6');
-      assert.fail(6, 3 * 2);
-      assert.fail(6 * '2', 12);
-      assert.fail([1, 'a', {}], [1, 'a', {}]);
+      assert.notStrictEqual(6, '6');
+      assert.strictEqual(6, 3 * 2);
+      assert.strictEqual(6 * '2', 12);
+      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
     // #7
     test('#deepEqual, #notDeepEqual', function () {
